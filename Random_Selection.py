d33e1653737b8044ec13bce314a2261ed2357154
taghva jalili <pietyjalili@gmail.com>
2025-03-09 18:10:13 +0330

    Random Selection v.1.1 has uploaded

--- Random_Selection.py
+++ Random_Selection.py
@@ -1,19 +1,18 @@
 import random
 from generations import chromosomeGen
 
-def random_selection(all_chromo,user_input):
+def random_selection(all_chromo,size_of_group):
     new_gen = []
 
-    while len(new_gen) < (user_input ):
+    while len(new_gen) < size_of_group :
         adam = random.choice(all_chromo)
-        if adam not in new_gen :
-            new_gen.append(adam)
+        new_gen.append(adam)
     return new_gen
 
 user_input = int(input("How many chromosomes do you want to be generated: "))
 size_of_group = int(input("How many chromosomes should the group have: "))
-gen = chromosomeGen(size_of_group)
+gen = chromosomeGen(user_input)
 all_chromo = gen.gene_generator()
 
 print(f"All of the generated chromosomes: {all_chromo}")
-print(f"The selected chromosome: {random_selection(all_chromo,user_input)}")
+print(f"The selected chromosome: {random_selection(all_chromo,size_of_group)}")
