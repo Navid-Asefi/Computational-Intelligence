d9baf89509e824ebf8c3e8d56bbdc02a9e8466ee
Navid <navidasefi@yahoo.com>
2025-04-07 15:09:30 +0330

    Made the generator flexible for creating binary genes

--- tools/generations.py
+++ tools/generations.py
@@ -1,15 +1,18 @@
 import random
 
-class ChromosomeGen:
 
-    def __init__(self, generations):
+class ChromosomeGen:
+    def __init__(self, generations, binary=False):
         self.community = []
         self.generations = generations
+        self.binary = binary
 
     def gene_generator(self):
         """Generates the required Chromosomes and their genes"""
         for _ in range(self.generations):
-            self.community.append([random.randint(0, 9) for _ in range(10)])
-            
-        return self.community
+            if self.binary:
+                self.community.append([random.randint(0, 1) for _ in range(10)])
+            else:
+                self.community.append([random.randint(0, 9) for _ in range(10)])
 
+        return self.community
