2827aea289c45daa7c95bfe1d1bb7822fe30ba95
Nima <Nimakhazayi7@gmail.com>
2025-04-12 21:57:52 +0330

    convex combination by partitioning and its simple arithmetic form v1.2

--- Simple-Crossover.py
+++ Simple-Crossover.py
@@ -1,7 +1,7 @@
 from tools.generations import ChromosomeGen
 import sys
 
-def recombination():
+def simple():
 
     chromosome = ChromosomeGen(2)
     parents = chromosome.gene_generator()
@@ -17,9 +17,12 @@ def recombination():
         if num > 10 or num < 0:
             raise ValueError
         
-        for j in range(0, 10-num):
-            child1.append(parents[0][j])
-            child2.append(parents[1][j])
+        if num == 1:
+            return simple_arithmetic(alpha, parents)
+        
+        for i in range(0, 10-num):
+            child1.append(parents[0][i])
+            child2.append(parents[1][i])
 
         for i in range(10-num, 10):
             child1.append(round((alpha*parents[0][i]) + ((1-alpha)*parents[1][i]), 2))
@@ -34,7 +37,30 @@ def recombination():
     return child1, child2, parents
 
 
-child1, child2, parents = recombination()
+def simple_arithmetic(alpha, parents):
+
+    child1 = []
+    child2 = []
+    try: 
+        indx = int(input("Which gene you desire to change (start your counting from 1): "))
+        if indx > 10 or indx < 0:
+            raise ValueError
+        
+        for i in range(10):
+            child1.append(parents[0][i])
+            child2.append(parents[1][i])
+
+        child1[indx-1] = round((alpha*parents[0][indx-1]) + ((1-alpha)*parents[1][indx-1]), 2)
+        child2[indx-1] = round((alpha*parents[1][indx-1]) + ((1-alpha)*parents[0][indx-1]), 2)
+
+    except ValueError:
+        print("Gene index is larger than the number of each chromosome gene")
+        sys.exit()
+
+    return child1, child2, parents
+
+
+child1, child2, parents = simple()
 
 print(f"The parents: {parents}\n ")
 print(f"The first child: {child1}\n")
