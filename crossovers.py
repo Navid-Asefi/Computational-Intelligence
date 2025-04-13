b74dd0c2f1855fdea15896142df6e934b93759a7
Navid <navidasefi@yahoo.com>
2025-04-13 16:33:33 +0330

    Merged some of the crossover methods in one class

---
+++ crossovers.py
@@ -0,0 +1,73 @@
+import random
+
+
+class Crossovers:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def one_point(parent1, parent2):
+        # pointer
+        p = random.randint(0, 9)
+        child1 = parent1[0:p] + parent2[p:10]
+        child2 = parent2[0:p] + parent1[p:10]
+        return child1, child2, p
+
+    @staticmethod
+    def two_point(parent1, parent2):
+        # pointer
+        p1 = random.randint(1, 9)
+        p2 = random.randint(1, 9)
+        while p1 == p2:
+            p2 = random.randint(1, 9)
+        if p1 > p2:
+            temp = p1
+            p1 = p2
+            p2 = temp
+
+        child1 = parent1[0:p1] + parent2[p1:p2] + parent1[p2:10]
+        child2 = parent2[0:p1] + parent1[p1:p2] + parent2[p2:10]
+        return child1, child2, p1, p2
+
+    @staticmethod
+    def simple_arithmetic(parent1, parent2, alpha, num):
+        child1 = []
+        child2 = []
+        for i in range(10):
+            child1.append(parent1[i])
+            child2.append(parent2[i])
+
+        child1[num - 1] = round(
+            (alpha * parent1[num - 1]) + ((1 - alpha) * parent2[num - 1]), 2
+        )
+        child2[num - 1] = round(
+            (alpha * parent2[num - 1]) + ((1 - alpha) * parent1[num - 1]), 2
+        )
+
+        return child1, child2
+
+    @staticmethod
+    def simple(parent1, parent2, alpha, num):
+        child1 = []
+        child2 = []
+
+        for i in range(0, 10 - num):
+            child1.append(parent1[i])
+            child2.append(parent2[i])
+
+        for i in range(10 - num, 10):
+            child1.append(round((alpha * parent1[i]) + ((1 - alpha) * parent2[i]), 2))
+            child2.append(round((alpha * parent2[i]) + ((1 - alpha) * parent1[i]), 2))
+
+        return child1, child2
+
+    @staticmethod
+    def whole_crossover(parent1, parent2, alpha):
+        child = [
+            round(alpha * a + (1 - alpha) * b, 2) for a, b in zip(parent1, parent2)
+        ]
+
+        child2 = [
+            round(alpha * a + (1 - alpha) * b, 2) for a, b in zip(parent2, parent1)
+        ]
+        return child, child2
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from crossovers import Crossovers
 from selection import Selection
@@ -48,7 +49,9 @@ def main():
     crossover_methods = {
         "1": ("One Point Crossover", "one_point"),
         "2": ("Two Point Crossover", "two_point"),
-        "3": ("Uniform Crossover", "uniform"),
+        "3": ("Simple Crossover", "simple"),
+        "4": ("Simple Arithmetic Crossover", "simple_arithmetic"),
+        "5": ("Whole Arithmetic Crossover", "whole_crossover"),
     }
 
     binary_crossovers = {}
@@ -109,9 +112,41 @@ def main():
     crossover_func = getattr(crossover, crossover_methods[crossover_choice][1])
 
     offspring = []
-    for i in range(0, len(selected) - 1, 2):
-        offspring.append(crossover_func(selected[i], selected[i + 1]))
+    if crossover_func_name == "simple":
+        alpha = get_float("Type the desired alpha (between 0 and 1): ")
+        num = get_int("How many genes to blend (0 to 10): ")
+        if not (0 <= alpha <= 1 and 0 <= num <= 10):
+            print("Invalid alpha or gene count.")
+            sys.exit()
 
+    elif crossover_func_name == "simple_arithmetic":
+        alpha = get_float("Type the desired alpha (between 0 and 1): ")
+        num = get_int("Type the gene number you want to change:  ")
+        if not (0 <= alpha <= 1 and 0 <= num <= 10):
+            print("Invalid alpha or gene count.")
+            sys.exit()
+
+    elif crossover_func_name == "whole_crossover":
+        alpha = get_float("Type the desired alpha (between 0 and 1): ")
+        if not (0 <= alpha <= 1):
+            print("Invalid alpha or gene count.")
+            sys.exit()
+
+    for i in range(0, len(selected) - 1, 2):
+        parent1 = selected[i]
+        parent2 = selected[i + 1]
+
+        if crossover_func_name in ("simple", "simple_arithmetic"):
+            child1, child2 = crossover_func(parent1, parent2, alpha, num)
+            offspring.extend([child1, child2])
+
+        elif crossover_func_name == "whole_crossover":
+            child1, child2 = crossover_func(parent1, parent2, alpha)
+            offspring.extend([child1, child2])
+
+        else:
+            child = crossover_func(parent1, parent2)
+            offspring.append(child)
     print("\nOffspring after crossover:")
     for i, child in enumerate(offspring):
         print(f"{i + 1}: {child}")
