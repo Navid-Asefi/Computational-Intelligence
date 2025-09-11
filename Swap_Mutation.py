212ab3b03bb2e3fac974fadb30190d58636675bb
taghva jalili <pietyjalili@gmail.com>
2025-09-11 20:06:36 +0330

    Swap Mutation has been added

---
+++ Swap_Mutation.py
@@ -0,0 +1,39 @@
+import random
+from tools.generations import ChromosomeGen
+
+chromosomes = ChromosomeGen(1)
+parents = chromosomes.gene_generator()
+parent=parents[0]
+
+def swap_mutation(parent):
+    """
+    Args:
+        chromosome (list): The parent chromosome to be mutated.
+        mutation_rate (float): The probability of mutation (e.g., 0.05 for 5%).
+
+    Returns:
+        list: The mutated offspring chromosome.
+    """
+    # Create a copy to avoid modifying the original
+    offspring = parent.copy()
+    
+    length = 10
+        
+    # Randomly select two distinct indices
+    idx1, idx2 = 0, 0
+    while idx1 == idx2:
+        idx1 = random.randint(0, 9)
+        idx2 = random.randint(0, 9)
+             
+    # Swap the values at the selected indices
+    offspring[idx1], offspring[idx2] = offspring[idx2], offspring[idx1]
+
+        
+    
+    return offspring
+
+
+
+offspring = swap_mutation(parent)
+print("Parent:   ", parent)
+print("Offspring:", offspring)
